31faaaa448fec1ee84ebf8471db78d72025a46c0
DN <d4417n@yandex.ru>
2020-05-24 12:52:38 +0300

    Добавление  Функции

--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
 //---метод прогрузки страницы дом-дерево
 window.addEventListener("DOMContentLoaded", function () {
   "use strict";
-  let tab = document.querySelectorAll(".info-header-tab");
-  //   console.log(tab);
-  let info = document.querySelector(".info-header");
-  //   console.log(info);
-  let tabContent = document.querySelectorAll(".info-tabcontent");
-  //   console.log(tabContent);
+  function useElement(dateA, dateB, dateC) {
+    dateA = document.querySelectorAll(".info-header-tab");
+    //   console.log(tab);
+    dateB = document.querySelector(".info-header");
+    //   console.log(info);
+    dateC = document.querySelectorAll(".info-tabcontent");
+    //   console.log(tabContent);
 
-  // функция скрытия табКонтента
-  function hideTabContent(a) {
-    for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove("show"); // метод удаление класса show скрытия его страницы
-      tabContent[i].classList.add("hide"); // добавление класса hide - скрыть
-    }
-  }
-
-  hideTabContent(1); // Скрытие с первого элемента использование аргумента (а)
-
-  // функция которая будет показывать табКонтент
-  function showTabContent(b) {
-    //проверка скрыт ли этот элемент
-    if (tabContent[b].classList.contains("hide")) {
-      tabContent[b].classList.remove("hide"); // удаление класса hide скрыть
-      tabContent[b].classList.add("show"); // добавление класса show показать
-    }
-  }
-
-  //Делигирование обработчика событий клика на определенном элементе с проверкой
-  info.addEventListener("click", function (event) {
-    let target = event.target;
-    if (target && target.classList.contains("info-header-tab")) {
-      for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) {
-          hideTabContent(0); // скрытие всех табов
-          showTabContent(i); // открытие того элемента на который кликнули
-          break;
-        }
+    // функция скрытия табКонтента
+    function hideTabContent(a) {
+      for (let i = a; i < dateC.length; i++) {
+        dateC[i].classList.remove("show"); // метод удаление класса show скрытия его страницы
+        dateC[i].classList.add("hide"); // добавление класса hide - скрыть
       }
     }
-  });
+
+    hideTabContent(1); // Скрытие с первого элемента использование аргумента (а)
+
+    // функция которая будет показывать табКонтент
+    function showTabContent(b) {
+      //проверка скрыт ли этот элемент
+      if (dateC[b].classList.contains("hide")) {
+        dateC[b].classList.remove("hide"); // удаление класса hide скрыть
+        dateC[b].classList.add("show"); // добавление класса show показать
+      }
+    }
+
+    //Делигирование обработчика событий клика на определенном элементе с проверкой
+    dateB.addEventListener("click", function (event) {
+      let target = event.target;
+      if (target && target.classList.contains("info-header-tab")) {
+        for (let i = 0; i < dateA.length; i++) {
+          if (target == dateA[i]) {
+            hideTabContent(0); // скрытие всех табов
+            showTabContent(i); // открытие того конткента который соответствует нажатому табу
+            break;
+          }
+        }
+      }
+    });
+  }
+  useElement();
 });
